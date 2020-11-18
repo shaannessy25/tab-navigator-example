055d64904553c593c6a8de5826fd9b4e95231902
shaannessy25 <shaan.hurley25@gmail.com>
2020-11-18 10:19:44 -0800

    adding icons to homepage

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { Ionicons } from 'react-native-vector-icons';
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }}>
+      <Text>Home</Text>
+      <Ionicons name="ios-add-circle-outline" size={32} />
     </View>
   );
 }
